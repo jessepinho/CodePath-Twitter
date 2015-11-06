4e8a7f55776fd8e9088390108be19c3ce5f613c9
Jesse Pinho <jesse@jessepinho.com>
2015-11-05 23:36:57 -0600

    Populate subviews with data

--- CodePath-Twitter/TweetViewController.m
+++ CodePath-Twitter/TweetViewController.m
@@ -7,14 +7,25 @@
 //
 
 #import "TweetViewController.h"
+#import "UIImageView+AFNetworking.h"
 
 @interface TweetViewController ()
-
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *profileImageView;
+@property (weak, nonatomic) IBOutlet UILabel *screenNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *tweetLabel;
 @end
 
 @implementation TweetViewController
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    self.title = @"Tweet";
+
+    self.nameLabel.text = self.tweet.user.name;
+    [self.profileImageView setImageWithURL:self.tweet.user.profileImageURL];
+    self.screenNameLabel.text = [NSString stringWithFormat:@"@%@", self.tweet.user.screenName];
+    self.tweetLabel.text = self.tweet.text;
+    [self.tweetLabel sizeToFit];
 }
 @end
